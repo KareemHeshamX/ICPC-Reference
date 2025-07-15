a739824b4cdf1fe01d5ed97f67f284e218ff2ec4
George1712 <84546695+George1712@users.noreply.github.com>
2025-07-15 15:19:12 +0300

    Changed the Trie template

--- strings/trie.h
+++ strings/trie.h
@@ -1,40 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class trie {
-	struct trie_node {
-		bool is_leaf = false;
-		map<char, int> next;
-		bool have_next(char ch) {
-			return next.find(ch) != next.end();
+struct trie {
+    trie* next[26]{};
+    bool endOfWord = false;
+    void insert(string &s) {
+        trie *current = this;
+        for (char &ch : s) {
+            int i = ch - 'a';
+            if (current -> next[i] == nullptr) {
+                current -> next[i] = new trie;
+            }
+            current = current -> next[i];
+        }
+        current -> endOfWord = true;
+    }
+    bool search(string &s) {
+        trie *current = this;
+        for (char &ch : s) {
+            int i = ch - 'a';
+            if (current -> next[i] == nullptr) {
+                return false;
+            }
+            current = current -> next[i];
+        }
+        return current -> endOfWord;
+    }
+	bool erase(string& s, int idx = 0) {
+		if (idx == s.size()) {
+			if (!endOfWord) return false;
+			endOfWord = false;
+			for (auto child : next)
+				if (child) return false;
+			return true;
 		}
-		int& operator[](char ch) {
-			return next[ch];
+		int i = s[idx] - 'a';
+		if (!next[i]) return false;
+		bool shouldDeleteChild = next[i]->erase(s, idx + 1);
+		if (shouldDeleteChild) {
+			delete next[i];
+			next[i] = nullptr;
+			for (auto child : next)
+				if (child) return false;
+			return !endOfWord;
 		}
-	};
-	vector<trie_node> t;
-public:
-	trie() {
-		t.push_back(trie_node());
-	}
-	void insert(const string &s) {
-		int root = 0;
-		for (const char &ch : s) {
-			if (!t[root].have_next(ch)) {
-				t.push_back(trie_node());
-				t[root][ch] = t.size() - 1;
-			}
-			root = t[root][ch];
-		}
-		t[root].is_leaf = true;
-	}
-	bool find(const string &s) {
-		int root = 0;
-		for (const char &ch : s) {
-			if (!t[root].have_next(ch))
-				return false;
-			root = t[root][ch];
-		}
-		return t[root].is_leaf;
+		return false;
 	}
 };
