a21ffbad320c2432825a86a1da39d86ff767f1f9
Kareem Hesham <kareem.hesham6671@gmail.com>
2026-01-21 13:33:02 +0200

    Optimized dp sos file

--- misc/dp sos.h
+++ misc/dp sos.h
@@ -1,18 +1,19 @@
 //iterative version
-for(int mask = 0; mask < (1<<N); ++mask){
-    dp[mask][-1] = A[mask];	//handle base case separately (leaf states)
-    for(int i = 0;i < N; ++i){
-        if(mask & (1<<i))
-            dp[mask][i] = dp[mask][i-1] + dp[mask^(1<<i)][i-1];
-        else
-            dp[mask][i] = dp[mask][i-1];
+vector<int> sos(1 << n);
+vector<vector<int>> dp(1 << n, vector<int>(n + 1));
+
+for (int x = 0; x < (1 << n); x++) {
+    dp[x][0] = a[x];
+    for (int i = 0; i < n; i++) {
+        dp[x][i + 1] = dp[x][i];
+        if (x & (1 << i)) { dp[x][i + 1] += dp[x ^ (1 << i)][i]; }
     }
-    F[mask] = dp[mask][N-1];
+    sos[x] = dp[x][n];
 }
 //memory optimized, super easy to code.
-for(int i = 0; i<(1<<N); ++i)
-    F[i] = A[i];
-for(int i = 0;i < N; ++i) for(int mask = 0; mask < (1<<N); ++mask){
-    if(mask & (1<<i))
-        F[mask] += F[mask^(1<<i)];
+sos = a;
+for (int i = 0; i < n; i++) {
+    for (int x = 0; x < (1 << n); x++) {
+        if (x & (1 << i)) { sos[x] += sos[x ^ (1 << i)]; }
+    }
 }
