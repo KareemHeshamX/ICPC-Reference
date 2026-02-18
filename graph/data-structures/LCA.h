3186eb7cba90b40828ced9cb2ea176c4caee8ed6
Kareem Hesham <kareem.hesham6671@gmail.com>
2026-02-18 14:35:01 +0200

    Modified LCA.h file

--- graph/data-structures/LCA.h
+++ graph/data-structures/LCA.h
@@ -1,44 +1,39 @@
 class LCA {
-	int n, logN, root = 1;
+public:
+	int n, logN, root;
 	vector<int> depth;
 	vector<vector<int>> adj, lca;
 	void dfs(int node, int parent) {
-		lca[node][0] = parent;
+		lca[0][node] = parent;
 		depth[node] = (~parent ? depth[parent] + 1 : 0);
 		for (int k = 1; k <= logN; k++) {
-			int up_parent = lca[node][k - 1];
-			if (~up_parent)
-				lca[node][k] = lca[up_parent][k - 1];
+			int up_parent = lca[k - 1][node];
+			if (~up_parent) lca[k][node] = lca[k - 1][up_parent];
 		}
 		for (int child : adj[node])
 			if (child != parent)
 				dfs(child, node);
 	}
-public:
-	LCA(const vector<vector<int>> &_adj, int root =1) :
-			root(root), adj(_adj) {
+	LCA(const vector<vector<int>> &_adj, int root = 1) : root(root), adj(_adj) {
 		adj = _adj;
 		n = adj.size() - 1;
 		logN = log2(n);
-		lca = vector<vector<int>>(n + 1, vector<int>(logN + 1, -1));
+		lca = vector<vector<int>>(logN + 1, vector<int>(n + 1, -1));
 		depth = vector<int>(n + 1);
 		dfs(root, -1);
 	}
 	int get_LCA(int x, int y) {
-		if (depth[x] < depth[y])
-			swap(x, y);
+		if (depth[x] < depth[y]) swap(x, y);
 		for (int k = logN; k >= 0; k--)
-			if (depth[x] - (1 << k) >= depth[y])
-				x = lca[x][k];
-		if (x == y)
-			return x;
+			if (depth[x] - (1 << k) >= depth[y]) x = lca[k][x];
+		if (x == y) return x;
 		for (int k = logN; k >= 0; k--) {
 			if (lca[x][k] != lca[y][k]) {
-				x = lca[x][k];
-				y = lca[y][k];
+				x = lca[k][x];
+				y = lca[k][y];
 			}
 		}
-		return lca[x][0];
+		return lca[0][x];
 	}
 	int get_distance(int u, int v) {
 		return depth[u] + depth[v] - 2 * depth[get_LCA(u, v)];
@@ -46,16 +41,15 @@ public:
     //Do you even lift, bro?
 	int kth_ancestor(int node, int dist) {
 		for (int i = logN; i >= 0 && ~node; i--)
-			if (dist & (1 << i))
-				node = lca[node][i];
+			if (dist & (1 << i)) node = lca[i][node];
 		return node;
 	}
 	edge get_path(int u, int LCA) {
 		edge rt;
 		for (int k = logN; k >= 0; k--)
 			if (depth[u] - (1 << k) >= depth[LCA]) {
-				rt = merge(rt, lca[u][k]);
-				u = lca[u][k].to;
+				rt = merge(rt, lca[k][u]);
+				u = lca[k][u].to;
 			}
 		return rt;
 	}
