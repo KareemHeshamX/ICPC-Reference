4a40f007fff199d01a228c14a0c07e715f12c25c
Kareem Hesham <kareem.hesham6671@gmail.com>
2026-02-28 15:12:42 +0200

    Updated Iterative SegmentTree.h file

--- data-structures/segment-tree/Iterative SegmentTree.h
+++ data-structures/segment-tree/Iterative SegmentTree.h
@@ -2,24 +2,25 @@ template <class T, class F = function<T(const T&, const T&)>>
 struct segment_tree {
     int n;
     vector<T> tree;
-    T unit; F f;
+    T neutral;
+    F merge;
 
     segment_tree() {}
-    segment_tree(int _n, T _unit, F _f) : n(1 << (__bit_width(_n))), tree(2 * n, _unit), unit(_unit), f(_f) {}
+    segment_tree(int _n, F _merge, T _neutral) : n(1 << (__bit_width(_n))), tree(2 * n, _neutral), neutral(_neutral), merge(_merge) {}
 
     void update(int k, T x) {
-        (tree[k += n] *= x) %= mod;
+        (tree[k += n] = x);
         for (k /= 2; k >= 1; k /= 2) {
-            tree[k] = f(tree[2 * k], tree[2 * k + 1]);
+            tree[k] = merge(tree[2 * k], tree[2 * k + 1]);
         }
     }
 
     T query(int l, int r) {
-        T ansl = unit, ansr = unit;
+        T ansl = neutral, ansr = neutral;
         for (l += n, r += n; l <= r; l /= 2, r /= 2) {
-            if (l & 1) ansl = f(ansl, tree[l++]);
-            if (!(r & 1)) ansr = f(tree[r--], ansr);
+            if (l & 1) ansl = merge(ansl, tree[l++]);
+            if (!(r & 1)) ansr = merge(tree[r--], ansr);
         }
-        return f(ansl, ansr);
+        return merge(ansl, ansr);
     }
 };
