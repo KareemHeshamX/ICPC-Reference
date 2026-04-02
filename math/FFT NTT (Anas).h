5e363d8a1eaad421ebe4ae375bc7dd05826abb76
Kareem Hesham <kareem.hesham6671@gmail.com>
2026-04-02 08:08:31 +0200

    Updated FFT NTT file

--- math/FFT NTT (Anas).h
+++ math/FFT NTT (Anas).h
@@ -1,55 +1,17 @@
 namespace FFT {
-    const int mod = 998244353; // 1451753786686046209
-    const int root = 3;
-    const int invRoot = 332748118;
- 
-    int fp(int b, int e) {
-        int res = 1;
-        while(e) {
-            if (e & 1) res = int(b * 1LL * res % mod);
-            b = int(b * 1LL * b % mod);
-            e >>= 1;
-        }
-        return res;
-    }
-    void primitiveRoot() {
-        int phi = mod - 1;
-        vector<int> fac;
-        for(int i = 2; i * 1LL * i < phi; i++) {
-            if(phi % i == 0) {
-                fac.push_back(i);
-                while(phi % i == 0) phi /= i;
-            }
-        }
-        if(phi > 1) fac.push_back(phi);
- 
-        for (int g = 2; g < mod; g++) {
-            for (int pr : fac) {
-                if (fp(g, (mod - 1) / pr) == 1) {
-                    goto bad;
-                }
-            }
-            cout << "const int root = " << g << ";\n";
-            cout << "const int invRoot = " << fp(g, mod - 2) << ";\n";
-            return;
-            bad:;
-        }
-        cerr << "404\n";
-    }
- 
     using cd = complex<double>;
     double pi = acos(-1);
- 
+
     void fft(vector<cd> &a, bool invert) {
         int n = (int)a.size();
- 
+
         for (int i = 1, j = 0; i < n; i++) {
             int bit = n >> 1;
             for(; j & bit; bit >>= 1) j ^= bit;
             j ^= bit;
             if(i < j) swap(a[i], a[j]);
         }
- 
+
         for (int len = 2; len <= n; len <<= 1) {
             double ang = 2 * pi / len * (invert ? -1 : 1);
             cd w1(cos(ang), sin(ang));
@@ -70,39 +32,80 @@ namespace FFT {
     }
     vector<int64_t> mul(const vector<int> &a, const vector<int> &b) {
         int N = 1 << (__lg(a.size() + b.size()) + 1);
- 
+
         vector<cd> ta(a.begin(), a.end()), tb(b.begin(), b.end());
         ta.resize(N);
         tb.resize(N);
- 
+
         fft(ta, false), fft(tb, false);
- 
+
         for(int i = 0; i < N; i++)
             ta[i] *= tb[i];
- 
+
         fft(ta, true);
- 
+
         vector<int64_t> ans(N);
         for(int i = 0; i < N; i++) {
             ans[i] = (int64_t)round(ta[i].real());
         }
- 
+
         return ans;
     }
- 
+}
+
+namespace NTT {
+    const int mod = 998244353; // 1451753786686046209
+    const int root = 3;
+    const int invRoot = 332748118;
+
+    int fp(int b, int e) {
+        int res = 1;
+        while(e) {
+            if (e & 1) res = int(b * 1LL * res % mod);
+            b = int(b * 1LL * b % mod);
+            e >>= 1;
+        }
+        return res;
+    }
+    // run local then change the root and the invRoot manually
+    void primitiveRoot() {
+        int phi = mod - 1;
+        vector<int> fac;
+        for(int i = 2; i * 1LL * i < phi; i++) {
+            if(phi % i == 0) {
+                fac.push_back(i);
+                while(phi % i == 0) phi /= i;
+            }
+        }
+        if (phi > 1) fac.push_back(phi);
+
+        for (int g = 2; g < mod; g++) {
+            for (int pr : fac) {
+                if (fp(g, (mod - 1) / pr) == 1) {
+                    goto bad;
+                }
+            }
+            cout << "const int root = " << g << ";\n";
+            cout << "const int invRoot = " << fp(g, mod - 2) << ";\n";
+            return;
+            bad:;
+        }
+        cerr << "404\n";
+    }
+
     void ntt(vector<int> &a, bool invert) {
         int n = (int)a.size();
- 
+
         for (int i = 1, j = 0; i < n; i++) {
             int bit = n >> 1;
             for (; j & bit; bit >>= 1) j ^= bit;
             j ^= bit;
             if (i < j) swap(a[i], a[j]);
         }
- 
+
         for (int len = 2; len <= n; len <<= 1) {
             int w1 = fp(invert ? invRoot : root, (mod - 1) / len);
- 
+
             for (int i = 0; i < n; i += len) {
                 int w = 1;
                 for (int j = 0; j < len / 2; j++) {
@@ -113,7 +116,7 @@ namespace FFT {
                 }
             }
         }
- 
+
         if (invert) {
             int n_1 = fp(n, mod - 2);
             for (int & x : a)
@@ -124,14 +127,14 @@ namespace FFT {
         int N = 1 << (__lg(a.size() + b.size()) + 1);
         a.resize(N);
         b.resize(N);
- 
+
         ntt(a, false), ntt(b, false);
- 
+
         for(int i = 0; i < N; i++)
             a[i] = int(a[i] * 1LL * b[i] % mod);
- 
+
         ntt(a, true);
- 
+
         return a;
     }
 }
