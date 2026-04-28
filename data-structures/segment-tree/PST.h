2abb02f6b43297e8a15857c9ffe196b2ec86d6d0
Kareem Hesham <kareem.hesham6671@gmail.com>
2026-04-28 10:33:29 +0300

    updated PST.h file

--- data-structures/segment-tree/PST.h
+++ data-structures/segment-tree/PST.h
@@ -3,10 +3,10 @@ typedef long long ll;
 using namespace std;
 
 struct Node {
-    ll val;
+    int val;
     Node *l, *r;
 
-    Node(ll x) : val(x), l(nullptr), r(nullptr) {}
+    Node(int x) : val(x), l(nullptr), r(nullptr) {}
     Node(Node *ll, Node *rr) {
         l = ll, r = rr;
         val = 0;
@@ -17,11 +17,10 @@ struct Node {
 };
 
 int n, cnt = 1;
-ll a[200001];
 Node *roots[200001];
 
 Node *build(int l = 1, int r = n) {
-    if (l == r) return new Node(a[l]);
+    if (l == r) return new Node(0);
     int mid = (l + r) / 2;
     return new Node(build(l, mid), build(mid + 1, r));
 }
@@ -30,10 +29,10 @@ Node *update(Node *node, int val, int pos, int l = 1, int r = n) {
     if (l == r) return new Node(val);
     int mid = (l + r) / 2;
     if (pos > mid) return new Node(node->l, update(node->r, val, pos, mid + 1, r));
-    else return new Node(update(node->l, val, pos, l, mid), node->r);
+    return new Node(update(node->l, val, pos, l, mid), node->r);
 }
 
-ll query(Node *node, int a, int b, int l = 1, int r = n) {
+int query(Node *node, int a, int b, int l = 1, int r = n) {
     if (l > b || r < a) return 0;
     if (l >= a && r <= b) return node->val;
     int mid = (l + r) / 2;
