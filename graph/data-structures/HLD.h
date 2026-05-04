855b212f4903dd4783ffd2459d7840862b22642d
Kareem Hesham <kareem.hesham6671@gmail.com>
2026-05-04 12:52:22 +0300

    Updated HLD.h file

--- graph/data-structures/HLD.h
+++ graph/data-structures/HLD.h
@@ -1,155 +1,156 @@
-// 1-based,if value in node,just update it after build chains
-// don't forget to call build_chains after add edges.
+struct Node {
+    int val = 0;
+} neutral;
 
-class HLD {
-	int n, is_value_in_edge;
-	vector<int> parent, depth, heavy, root, pos_in_array, pos_to_node, size;
-	struct node {
-		int b = -1e9, w = b;
-		node() {}
-		node (int _w, int _b) : b(_b), w(_w) {}
-		const node operator+(const node &other) const {
-			node ret = {b + other.b, w + other.w};
-			return ret;
-		};
-		node (node &x, node &y) {b = x.b + y.b, w = x.w + y.w;}
-	};
-	struct segment_tree {
-		int n;
-		vector<node> tree;
-		node neutral;
-		node merge (node &a, node &b) {return a + b;}
+struct segtree {
+    segtree *left = nullptr, *right = nullptr;
 
-		segment_tree() {}
-		segment_tree(int _n) : n(1 << (__bit_width(_n))), tree(2 * n, neutral) {}
+    Node node = {};
 
-		void update(int k, node x) {
-			(tree[k += n] = x);
-			for (k /= 2; k >= 1; k /= 2) {
-				tree[k] = merge(tree[2 * k], tree[2 * k + 1]);
-			}
-		}
+    int start, end;
 
-		node query(int l, int r) {
-			node ansl = neutral, ansr = neutral;
-			for (l += n, r += n; l <= r; l /= 2, r /= 2) {
-				if (l & 1) ansl = merge(ansl, tree[l++]);
-				if (!(r & 1)) ansr = merge(tree[r--], ansr);
-			}
-			return merge(ansl, ansr);
-		}
-	} seg;
-	struct TREE {
-		int cnt_edges = 1;
-		vector<vector<int>> adj;
-		//need for value in edges
-		vector<vector<int>> edge_idx;
-		//edge_to need for undirected tree //end of edge in directed tree
-		vector<int> edge_to, edge_cost;
-		TREE(int n) :
-				adj(n + 1), edge_idx(n + 1), edge_to(n + 1), edge_cost(n + 1) {
-		}
-		void add_edge(int u, int v, int c) {
-			adj[u].push_back(v);
-			adj[v].push_back(u);
-			edge_idx[u].push_back(cnt_edges);
-			edge_idx[v].push_back(cnt_edges);
-			edge_cost[cnt_edges] = c;
-			cnt_edges++;
-		}
-	} tree;
-	int dfs_hld(int node) {
-		int size = 1, max_sub_tree = 0;
-		for (int i = 0; i < (int) tree.adj[node].size(); i++) {
-			int ch = tree.adj[node][i], edge_idx = tree.edge_idx[node][i];
-			if (ch != parent[node]) {
-				tree.edge_to[edge_idx] = ch;
-				parent[ch] = node;
-				depth[ch] = depth[node] + 1;
-				int child_size = dfs_hld(ch);
-				if (child_size > max_sub_tree)
-					heavy[node] = ch, max_sub_tree = child_size;
-				size += child_size;
-			}
-		}
-		return size;
-	}
-	vector<tuple<int, int, bool>> get_path(int u, int v) { //l,r,must_reverse?
-		vector<pair<int, int>> tmp[2];
-		bool idx = 1;
-		while (root[u] != root[v]) {
-			if (depth[root[u]] > depth[root[v]]) {
-				swap(u, v);
-				idx = !idx;
-			}
-			//if value in edges ,you need value of root[v] also (connecter edge)
-			tmp[idx].push_back( { pos_in_array[root[v]], pos_in_array[v] });
-			v = parent[root[v]];
-		}
-		if (depth[u] > depth[v]) {
-			swap(u, v);
-			idx = !idx;
-		}
-		if (!is_value_in_edge || u != v)
-			tmp[idx].push_back( { pos_in_array[u] + is_value_in_edge,
-					pos_in_array[v] });
-		reverse(tmp[1].begin(), tmp[1].end());
-		vector<tuple<int, int, bool>> rt;
-		for (int i = 0; i < 2; i++)
-			for (auto &it : tmp[i])
-				rt.emplace_back(it.first, it.second, i == 0);
-		return rt; //u is LCA
-	}
-public:
-	HLD(int n, bool is_value_in_edge) :
-			n(n), is_value_in_edge(is_value_in_edge), seg(n + 1), tree(n + 1) {
-		heavy = vector<int>(n + 1, -1);
-		parent = depth = root = pos_in_array = pos_to_node = size = vector<int>(
-				n + 1);
-	}
-	void add_edge(int u, int v, int c = 0) {
-		tree.add_edge(u, v, c);
-	}
-	void build_chains(int src = 1) {
-		parent[src] = -1;
-		dfs_hld(src);
-		for (int chain_root = 1, pos = 1; chain_root <= n; chain_root++) {
-			if (parent[chain_root] == -1
-					|| heavy[parent[chain_root]] != chain_root)
-				for (int j = chain_root; j != -1; j = heavy[j]) {
-					root[j] = chain_root;
-					pos_in_array[j] = pos++;
-					pos_to_node[pos_in_array[j]] = j;
-				}
-		}
-		if (is_value_in_edge)
-			for (int i = 1; i < n; i++)
-				update_edge(i, tree.edge_cost[i], -1e9);
-	}
-	void update_node(int node, int val1, int val2) {
-		seg.update(pos_in_array[node], {val1, val2});
-	}
-	void update_edge(int edge_idx, int val1, int val2) {
-		update_node(tree.edge_to[edge_idx], val1, val2);
-	}
-	void update_path(int u, int v, ll c) {
-		vector<tuple<int, int, bool>> intervals = get_path(u, v);
-		for (auto &it : intervals)
-			seg.update(get<0>(it), get<1>(it), c);
-	}
-	node query_in_path(int u, int v) {
-		vector<tuple<int, int, bool>> intervals = get_path(u, v);
-		//initial value,check if handling u == v
-		node query_res;
-		for (auto &it : intervals) {
-			int l, r;
-			bool rev;
-			tie(l, r, rev) = it;
-			node cur = seg.query(l, r);
-			if (rev)
-				cur.reverse();
-			query_res = node(query_res, cur);
-		}
-		return query_res;
-	}
+    segtree(int l = 0, int r = 0) : start(l), end(r) {}
+
+    void extend() {
+        if (left == nullptr) {
+            int mid = start + end >> 1;
+            left = new segtree(start, mid);
+            right = new segtree(mid + 1, end);
+        }
+    }
+
+    Node pushup(Node a, Node b) {
+        Node ret;
+        ret.val = a.val + b.val;
+        return ret;
+    }
+
+    void update(int idx, int val) {
+        if (start > idx || end < idx)
+            return;
+        if (start == end) {
+            node.val = val;
+            return;
+        }
+        extend();
+        left->update(idx, val);
+        right->update(idx, val);
+        node = pushup(left->node, right->node);
+    }
+
+    Node query(int l, int r) {
+        if (r < start || end < l)
+            return neutral;
+        extend();
+        if (l <= start && end <= r)
+            return node;
+        Node ret = pushup(left->query(l , r), right->query(l , r));
+        return ret;
+    }
+
+    ~segtree() {
+        if (left == nullptr)return;
+        delete left;
+        delete right;
+    }
+};
+
+
+struct HLD{
+    int n;
+    vector<int> par, depth, heavy, head, cnt, pos_array, pos_node, val, edge_node;
+    vector<vector<int>> adj;
+    bool value_on_edge = false;
+    segtree* seg;
+
+    HLD(vector<int>& val) : n(n), adj(adj){
+        init(val);
+    }
+
+    HLD(int n, vector<vector<array<int, 3>>>& adj) : n(n){
+        vector<int> val(n + 1);
+        value_on_edge = true;
+        this->adj = vector<vector<int>>(n + 1);
+        this->edge_node = vector<int>(n + 1);
+        auto direct_edge = [&](auto& self, int u, int p) -> void{
+            for(auto& [v, w, idx] : adj[u]){
+                if(v == p) continue;
+                edge_node[idx] = v;
+                this->adj[u].push_back(v);
+                val[v] = w;
+                self(self, v, u);
+            }
+        };
+        direct_edge(direct_edge, 1, -1);
+        init(val);
+    }
+
+    void init(vector<int>& val){
+        depth = head = cnt = pos_array = pos_node = vector<int>(n + 1);
+        heavy = par = vector<int>(n + 1, -1);
+        this->val = val;
+        decompose(1);
+    }
+
+    void dfs_hld(int u){
+        cnt[u] = 1;
+        int max_size = 0;
+        for(auto v : adj[u]){
+            if(v == par[u]) continue;
+            par[v] = u; depth[v] = depth[u] + 1;
+            dfs_hld(v);
+            cnt[u] += cnt[v];
+            if(max_size < cnt[v]){
+                max_size = cnt[v];
+                heavy[u] = v;
+            }
+        }
+    }
+
+    void decompose(int root = 1){
+        dfs_hld(root);
+        int nxt = 0;
+        for (int chain_root = 1, pos = 1; chain_root <= n; chain_root++){
+            if(par[chain_root] == -1 || heavy[par[chain_root]] != chain_root){
+                for(int ch = chain_root; ~ch; ch = heavy[ch]){
+                    head[ch] = chain_root;
+                    pos_array[ch] = nxt;
+                    pos_node[nxt++] = ch;
+                }
+            }
+        }
+        seg = new segtree(1, n);
+        for(int u = 1; u <= n; u++){
+            seg->update(pos_array[u], val[u]);
+        }
+    }
+
+    Node query(int u, int v) {
+        Node ans = neutral;
+        for(; head[u] != head[v]; v = par[head[v]]){
+            if(depth[head[u]] > depth[head[v]])
+                swap(u, v);
+            Node cur = seg->query(pos_array[head[v]], pos_array[v]);
+            ans = seg->pushup(ans, cur);
+        }
+        if (depth[u] > depth[v])
+            swap(u, v);
+        if(pos_array[u] + value_on_edge <= pos_array[v]){
+            Node last_chain = seg->query(pos_array[u] + value_on_edge, pos_array[v]);
+            ans = seg->pushup(ans, last_chain);
+        }
+        return ans;
+    }
+
+    void update(int u, int c){
+        seg->update(pos_array[u], c);
+    }
+
+    void update_edge(int edge_idx, int c){
+        update(edge_node[edge_idx], c);
+    }
+
+    ~HLD(){
+        delete seg;
+    }
 };
