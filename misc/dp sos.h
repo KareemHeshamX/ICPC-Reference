63d7925d6c88c2e44b5f9439d8e323bf6a802182
Kareem Hesham <kareem.hesham6671@gmail.com>
2026-05-05 17:10:17 +0300

    updated dp sos

--- misc/dp sos.h
+++ misc/dp sos.h
@@ -1,19 +1,46 @@
-//iterative version
-vector<int> sos(1 << n);
-vector<vector<int>> dp(1 << n, vector<int>(n + 1));
+const int LG = 22;
+const int M = 1 << LG;
+// subset contribute to its superset
 
-for (int x = 0; x < (1 << n); x++) {
-    dp[x][0] = a[x];
-    for (int i = 0; i < n; i++) {
-        dp[x][i + 1] = dp[x][i];
-        if (x >> i & 1) { dp[x][i + 1] += dp[x ^ (1 << i)][i]; }
+void forward1(vector<ll>&dp) {
+    for (int bt = 0; bt < LG; ++bt) {
+        for (int m = 0; m < M; ++m) {
+            if (m >> bt & 1){
+                dp[m] += dp[m ^ (1 << bt)];
+            }
+        }
     }
-    sos[x] = dp[x][n];
 }
-//memory optimized, super easy to code.
-sos = a;
-for (int i = 0; i < n; i++) {
-    for (int x = 0; x < (1 << n); x++) {
-        if (x >> i & 1) { sos[x] += sos[x ^ (1 << i)]; }
+// superset contribute to its subset
+
+void forward2(vector<ll>&dp) {
+    for (int bt = 0; bt < LG; ++bt) {
+        for (int m = M - 1; m >= 0; m--) {
+            if (m >> bt &1){
+                dp[m ^ (1 << bt)] += dp[m];
+            }
+        }
+    }
+}
+// remove subset contribution from superset
+
+void backward1(vector<ll>&dp) {
+    for (int bt = 0; bt < LG; bt++){
+        for (int m = M - 1; m >= 0; m--){
+            if (m >> bt &1){
+                dp[m] -= dp[m ^ (1 << bt)];
+            }
+        }
+    }
+}
+// remove superset contribution from subset
+
+void backward2(vector<ll> &dp) {
+    for (int bt = 0; bt < LG; bt++){
+        for (int m = 0; m < M; m++){
+            if (m >> bt &1){
+                dp[m ^ (1 << bt)] -= dp[m];
+            }
+        }
     }
 }
