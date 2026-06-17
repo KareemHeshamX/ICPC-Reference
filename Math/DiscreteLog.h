fa6ca6e71f9844c00c1e38cb5b9545cec3fd36d1
George Adel <georgeadel.cs@gmail.com>
2026-06-17 10:57:49 +0300

    added discrete log function to find the minimum x

--- Math/DiscreteLog.h
+++ Math/DiscreteLog.h
@@ -1,14 +1,15 @@
 int power(int a, int b, int m) {
     int res = 1;
     while (b > 0) {
-        if (b & 1) res = (res * 1ll * a) % m;
-        a = (a * 1ll * a) % m;
+        if (b & 1) res = (1LL * res * a) % m;
+        a = (1LL * a * a) % m;
         b >>= 1;
     }
     return res;
 }
 
-int solve(int a, int b, int m) {
+// return one of the possible solutions
+int discrete_log(int a, int b, int m) {
     a %= m, b %= m;
     int n = sqrt(m) + 1;
     unordered_map<int, int> vals;
@@ -24,12 +25,38 @@ int solve(int a, int b, int m) {
     return -1;
 }
 
-// when a and m are not coprime
+// Returns minimum x for which a ^ x % m = b % m, a and m are coprime.
+int discrete_log(int a, int b, int m) {
+    a %= m, b %= m;
+    int n = sqrt(m) + 1;
+
+    int an = 1;
+    for (int i = 0; i < n; ++i)
+        an = (an * 1ll * a) % m;
+
+    unordered_map<int, int> vals;
+    for (int q = 0, cur = b; q <= n; ++q) {
+        vals[cur] = q;
+        cur = (cur * 1ll * a) % m;
+    }
+
+    for (int p = 1, cur = 1; p <= n; ++p) {
+        cur = (cur * 1ll * an) % m;
+        if (vals.count(cur)) {
+            int ans = n * p - vals[cur];
+            return ans;
+        }
+    }
+    return -1;
+}
+
+
+// (General Solution) when a and m are not coprime
 // Returns minimum x for which a ^ x % m = b % m.
-int solve(int a, int b, int m) {
+int discrete_log(int a, int b, int m) {
     a %= m, b %= m;
     int k = 1, add = 0, g;
-    while ((g = gcd(a, m)) > 1) {
+    while ((g = __gcd(a, m)) > 1) {
         if (b == k)
             return add;
         if (b % g)
