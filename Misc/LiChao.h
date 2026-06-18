0ed2d4f9752887431e247670e368c0f86b6c43f9
Kareem Hesham <kareem.hesham6671@gmail.com>
2026-06-18 15:10:12 +0300

    fixed LiChao.h file

--- Misc/LiChao.h
+++ Misc/LiChao.h
@@ -1,12 +1,14 @@
-const int LC_N = (int)1e6 + 1;
-const long long LC_INF = (long long)1e17;
-vector<array<long long,2>> lc_tree(4 * LC_N, {0, LC_INF});
+#define ll long long
 
-long long f_line(const array<long long,2>& line, int x) {
+const int LC_N = (int)1e6 + 1;
+const ll LC_INF = (ll)1e17;
+vector<array<ll,2>> lc_tree(4 * LC_N, {0, LC_INF});
+
+ll f_line(const array<ll,2>& line, int x) {
     return line[0] * x + line[1];
 }
 
-void lc_insert(array<long long,2> line, int lo = 1, int hi = LC_N, int i = 1) {
+void lc_insert(array<ll,2> line, int lo = 1, int hi = LC_N, int i = 1) {
     int m = (lo + hi) / 2;
     bool left = f_line(line, lo) < f_line(lc_tree[i], lo);
     bool mid  = f_line(line, m)  < f_line(lc_tree[i], m);
@@ -20,9 +22,9 @@ void lc_insert(array<long long,2> line, int lo = 1, int hi = LC_N, int i = 1) {
         lc_insert(line, m, hi, 2 * i + 1);
 }
 
-long long lc_query(int x, int lo = 1, int hi = LC_N, int i = 1) {
+ll lc_query(int x, int lo = 1, int hi = LC_N, int i = 1) {
     int m = (lo + hi) / 2;
-    long long curr = f_line(lc_tree[i], x);
+    ll curr = f_line(lc_tree[i], x);
 
     if (hi - lo == 1) return curr;
 
