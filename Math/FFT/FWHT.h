775f007d1f219b339a06f57f9cf24009e196b0bd
George Adel <georgeadel.cs@gmail.com>
2026-06-23 17:39:10 +0300

    updating FWHT template

--- Math/FFT/FWHT.h
+++ Math/FFT/FWHT.h
@@ -1,13 +1,20 @@
-int add(int a, int b) {
-    return (a + b) % mod;
+const int mod = 1e9 + 7;
+
+inline int add(int a, int b) {
+    int res = a + b;
+    if (res >= mod) res -= mod;
+    return res;
 }
 
-int sub(int a, int b) {
-    return (a - b + mod) % mod;
+inline int sub(int a, int b) {
+    int res = a - b;
+    if (res < 0) res += mod;
+    return res;
 }
 
-ll poww(ll a, ll b) {
-    ll ret = 1;
+int64_t power(int64_t a, int64_t b) {
+    int64_t ret = 1;
+    a %= mod;
     while (b) {
         if (b & 1) ret = ret * a % mod;
         a = a * a % mod;
@@ -23,16 +30,13 @@ void fwht(vector<int> &a, int inv, int f) {
             for (int j = 0; j < len; j++) {
                 int x = a[i + j];
                 int y = a[i + j + len];
-
                 if (f == 0) {
-                    if (!inv)  a[i + j] = y, a[i + j + len] = add(x,  y);
-                    else  a[i + j] = sub(y, x), a[i + j + len] = x;
-                }
-                else if (f == 1) {
-                    if (!inv)  a[i + j + len] = add(x, y);
-                    else  a[i + j + len] = sub(y, x);
-                }
-                else {
+                    if (!inv) a[i + j] = add(x, y);
+                    else a[i + j] = sub(x, y);
+                } else if (f == 1) {
+                    if (!inv) a[i + j + len] = add(y, x);
+                    else a[i + j + len] = sub(y, x);
+                } else {
                     a[i + j] = add(x, y);
                     a[i + j + len] = sub(x, y);
                 }
@@ -43,14 +47,15 @@ void fwht(vector<int> &a, int inv, int f) {
 
 vector<int> mul(vector<int> a, vector<int> b, int f) { // 0:AND, 1:OR, 2:XOR
     int sz = a.size();
-    fwht(a, 0, f);  fwht(b, 0, f);
+    fwht(a, 0, f);  
+    fwht(b, 0, f);
     vector<int> c(sz);
     for (int i = 0; i < sz; ++i) {
         c[i] = 1ll * a[i] * b[i] % mod;
     }
     fwht(c, 1, f);
-    if (f) {
-        int sz_inv = poww(sz, mod - 2);
+    if (f == 2) { 
+        int sz_inv = power(sz, mod - 2);
         for (int i = 0; i < sz; ++i) {
             c[i] = 1ll * c[i] * sz_inv % mod;
         }
